778380a15fcda2773424aaccc3aba964c72818ba
IsaharStokhammer <8526656@gmail.com>
2024-08-19 17:51:22 +0300

    Add project files.

---
+++ חנות פלאפונים/IPhone.cs
@@ -0,0 +1,13 @@
+﻿namespace חנות_פלאפונים
+{
+    public interface IPhone
+    {
+        string Name { get; }
+        string Company { get; }
+        string Price { get; }
+        int Post (string Name,  string Price, string Company);
+        int Get (int Id);
+        int Put(int Id, IPhone phone);
+        int Delete (int Id);
+    }
+}
---
+++ חנות פלאפונים/Models/SmartPhone.cs
@@ -0,0 +1,28 @@
+﻿namespace חנות_פלאפונים.Models
+{
+    public class SmartPhone : IPhone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company {  get; set; }
+        public int price { get; set; }
+        public int Post(string Name, string Price, string Company)
+        {
+            return Id;
+        }
+        public int Get(int Id)
+        {
+            return Id;
+        }
+        public int Put(int Id, IPhone phone)
+        {
+            return Id;
+        }
+        public int Delete(int Id)
+        {
+            return Id;
+        }
+
+
+    }
+}
